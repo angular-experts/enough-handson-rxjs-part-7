533e03a60e7f734b651b23eafe10dc962b31d3a5
anas.r.firdousi <anas.firdousi@gmail.com>
2017-06-24 23:58:02 -0700

    added basic concatMap example

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 // Part 7
 
 console.log('----------RxJS: Transform Operators : ConcatMap, MergeMap & SwitchMap (with variations)-------------');
+
+// ConcatMap : Map values to inner observable, subscribe and emit value "in order"
+// ConcatMap : Maps each value to an Observable, then flattens all of these inner Observables using concatAll.
+
+const click$ = Rx.Observable.fromEvent(document, 'click');
+
+const result$ = click$.concatMap( ev => Rx.Observable.interval(2000));
+
+result$.subscribe( x =>  console.log(x));
+
+
+// MergeMap : Map values to inner observable, subscribe and emit value "as they appear"
